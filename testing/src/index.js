387caee6483ab8ef6bdf6608c0c455605e2bd528
DylanHedges <d.hedges1@hotmail.co.uk>
2020-01-14 08:15:01 -0500

    Removed src files, created boilerplate components for comment app

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './Components/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Renders the app to the DOM element with an id of root in the index.html file
+ReactDOM.render(<App />, document.querySelector('#root'));
