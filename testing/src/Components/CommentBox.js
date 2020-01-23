f6d8dc076c71c827538992fa817c00080f3e357c
DylanHedges <d.hedges1@hotmail.co.uk>
2020-01-23 06:57:59 -0500

    Refactored CommentBox to class based component - access component level state & watch user import

--- testing/src/Components/CommentBox.js
+++ testing/src/Components/CommentBox.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => {
-  return <div>Comment Box</div>
-};
+//Component that allows users to write comments - refactored to class based component so we can access component level state and watch input from a user
+class CommentBox extends Component {
+  render(){
+    return(
+      <form>
+        <h4>Add a comment</h4>
+        <textarea />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default CommentBox;
