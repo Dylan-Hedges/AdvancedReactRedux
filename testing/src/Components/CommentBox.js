4cbfc220c7ca8cb59edb80f6bfb9316a186867a4
DylanHedges <d.hedges1@hotmail.co.uk>
2020-01-27 07:44:30 -0500

    Added state for the component, added event handler that updates state with what user types, added on form submit event handler (resets text)

--- testing/src/Components/CommentBox.js
+++ testing/src/Components/CommentBox.js
@@ -2,16 +2,31 @@ import React, {Component} from 'react';
 
 //Component that allows users to write comments - refactored to class based component so we can access component level state and watch input from a user
 class CommentBox extends Component {
+  state = { comment: ''};
+
+  //Typing event handler - updates state based on what user types
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value});
+  };
+
+  //On submit form event handler
+  handleSubmit = event => {
+    //Prevents page reloading when user clicks submit
+    event.preventDefault();
+    //Removes all on screen text when user clicks submit - resets the state back to blank string which is reflected on screen
+    this.setState({ comment: ''});
+  };
+
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment}/>
         <div>
           <button>Submit Comment</button>
         </div>
       </form>
-    )
+    );
   }
 }
 
