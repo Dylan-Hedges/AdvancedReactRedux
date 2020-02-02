757917e4f096db25d6618bc08ffff990c85ab065
DylanHedges <d.hedges1@hotmail.co.uk>
2020-02-02 08:00:15 -0500

    Added find a textarea and button tests - searches in CommentBox component

--- testing/src/Components/__tests__/CommentBox.test.js
+++ testing/src/Components/__tests__/CommentBox.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import CommentBox from 'Components/CommentBox';
 
 it('has a text area and a button', () => {
-
+  //Renders CommentBox using the Enzyme mount function - renders the component and all of its children to a fake DOM (uses jsdom library)
+  const wrapped = mount(<CommentBox />);
+  //Checks to see if a textarea exists in the wrapped CommentBox component
+  expect(wrapped.find('textarea').length).toEqual(1);
+  //Checks to see if a button exists in the wrapped CommentBox component
+  expect(wrapped.find('button').length).toEqual(1);
 });
