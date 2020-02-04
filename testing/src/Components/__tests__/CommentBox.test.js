5a55c2ebdf7923d5538e7606b7525e3b86e746f0
DylanHedges <d.hedges1@hotmail.co.uk>
2020-02-03 20:55:54 -0500

    Added beforeEach() - executes before each it statement (mounts component) and afterEach() - executes after each it statement (unmounts/removes component as part of clean up)

--- testing/src/Components/__tests__/CommentBox.test.js
+++ testing/src/Components/__tests__/CommentBox.test.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'Components/CommentBox';
 
-it('has a text area and a button', () => {
+//Declares variable as part of the global scope
+let wrapped;
+
+//Executes an arrow function before each it statement
+beforeEach(() => {
   //Renders CommentBox using the Enzyme mount function - renders the component and all of its children to a fake DOM (uses jsdom library)
-  const wrapped = mount(<CommentBox />);
+  wrapped = mount(<CommentBox />);
+});
+
+//Executes an arrow function after each it statement
+afterEach(() => {
+  //Removes (unmounts) the component from the fake DOM - clean up, prevents component interfering with components in other tests
+  wrapped.unmount();
+});
+
+//Executes test - find textarea & find button
+it('has a text area and a button', () => {
   //Checks to see if a textarea exists in the wrapped CommentBox component
   expect(wrapped.find('textarea').length).toEqual(1);
   //Checks to see if a button exists in the wrapped CommentBox component
