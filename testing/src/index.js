9a274153f1ebe40e56d4062c2f98e0399ce0b215
DylanHedges <d.hedges1@hotmail.co.uk>
2020-02-17 07:46:15 -0500

    Wired up Redux to React - created redux store, wrapped app in the store, passed in Reducers

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from 'reducers';
 
 import App from 'Components/App';
 
 //Renders the app to the DOM element with an id of root in the index.html file
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  //Creates the Redux Store and wrapps it in the App component - passes in reducers and sets inital state to a blank object
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>
+  ,
+  document.querySelector('#root'));
