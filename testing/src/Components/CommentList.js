8d22f742c116ea303546872c6eb9226e77472d21
DylanHedges <d.hedges1@hotmail.co.uk>
2020-03-11 07:34:15 -0400

    Changed CommentList component from functional to class-based, wired up to redux store, added map function to map through comments and display on screen

--- testing/src/Components/CommentList.js
+++ testing/src/Components/CommentList.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>
-};
+//Component that renders comments on screen - class based
+class CommentList extends Component {
+  //Maps through comments and creates an <li> for each
+  renderComments(){
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+  //Displays comments on screen
+  render(){
+    return(
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    );
+  }
+}
+//Maps the comments in redux store to the component (see reducers/index.js for state.comments)
+function mapStateToProps(state){
+  return {comments: state.comments};
+}
+
+//Exports the component and wires it up to the redux store
+export default connect(mapStateToProps)(CommentList);
