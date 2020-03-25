86d43dc641809c4f4304a5ea641e51cea6e95071
DylanHedges <d.hedges1@hotmail.co.uk>
2020-03-25 19:38:41 -0400

    Added new reducer for fetch comments (dummy data) - added button to call fetchComments action creator, added FETCH_COMMENTS reducer that maps of payload of dummy data, extracts the name and saves to new array, new array is added to current state using spread operator

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import {SAVE_COMMENT, FETCH_COMMENTS} from 'actions/types';
 
-//AC - creates an action with the type of SAVE_COMMENT and the payload as the comment the user made
+//Action creator - returns user comment
 export function saveComment(comment){
+  //Returns an action with a payload containing the comment a user made
   return{
     type: SAVE_COMMENT,
     payload: comment
   };
 }
 
-
+//Action Creator - returns dummy comment data
 export function fetchComments(){
+  //Makes asynchronous request for dummy comments data
   const response = axios.get('http://jsonplaceholder.typicode.com/comments');
+  //Returns an action with a payload containing the dummy comments
   return {
     type: FETCH_COMMENTS,
     payload: response
--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,11 +1,18 @@
-import {SAVE_COMMENT} from 'actions/types';
+import {SAVE_COMMENT, FETCH_COMMENTS} from 'actions/types';
 //Reducer for list of comments
 export default function(state = [], action){
   //Looks at the actions type and updates state accordingly
   switch (action.type){
-    //Adds new comment to state - creates a new array, takes the current state and adds the comment (action.payload)
+    //Adds new comment to state
     case SAVE_COMMENT:
+      //Creates a new array, takes the current state and adds the comment (action.payload)
       return [...state, action.payload];
+    //Adds comments dummy data to state
+    case FETCH_COMMENTS:
+      //Maps over dummy data and extracts name, saves to new array
+      const comments = action.payload.data.map(comment => comment.name);
+      //Saves new array of names to state - uses spread operator
+      return [...state, ...comments]
     default:
       return state;
   }
