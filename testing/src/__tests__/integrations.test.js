9de4ec17f481c5325c5cec041644de1371226712
DylanHedges <d.hedges1@hotmail.co.uk>
2020-04-13 20:31:03 -0400

    Added moxios, set up moxios in the integration test file to intercept the fetch comments acios request - when using JSDOM (simulated browser) outside axios requests fail, moxios intercepts axios requests for a specific URL and returns a sucessful code (200) and dummy data (in this case comments) to axios

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import {mount} from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
+
 //Integration test - clicks te fetch comments button and checks comments are returend
 it('can fetch a list of comments and display them', () => {
+
+  //Executes before each test
+  beforeEach(() => {
+    //Installs moxios before each test
+    moxios.install();
+    //Tricks axios into thinking it got a response - intercepts axios requests for the below URL, then returns a successful code (200) and some dummy data to axios
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments',{
+      status: 200,
+      response: [{name: 'Fetched #1'},{name: 'Fetched #2'}]
+    });
+  });
+
+  //Executes after each test
+  afterEach(() => {
+    //Unistalls moxios, prevents us unintentally using the settings defined above for other tests
+    moxios.install();
+  });
 
   //Renders the App
   const wrapped = mount(
@@ -17,5 +36,5 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click');
 
   //Checks if there is a list of 500 comments
-  expect(wrapped.find('li').length).toEqual(500);
+  expect(wrapped.find('li').length).toEqual(2);
 });
