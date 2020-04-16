21b0903aff11a931f3be1a5c85fc88f1a3046f5f
DylanHedges <d.hedges1@hotmail.co.uk>
2020-04-16 07:58:51 -0400

    Added React Router

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {BrowserRouter, Route} from 'react-router-dom';
 import Root from 'Root';
 import App from 'Components/App';
 
 //Renders the app to the DOM element with an id of root in the index.html file; Root creates the Redux Store and connects the <App> component to it using the Provider tag - Root can be reused for any component to connect it to the Redux Store;
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>
   ,
   document.querySelector('#root'));
