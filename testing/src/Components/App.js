67336569e24336684ffd7dcfcce29621ff821c5d
DylanHedges <d.hedges1@hotmail.co.uk>
2020-04-17 10:17:26 -0400

    Added React Router routes for comment list and comment box components

--- testing/src/Components/App.js
+++ testing/src/Components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import CommentBox from 'Components/CommentBox';
 import CommentList from 'Components/CommentList';
 
 export default () => {
   return(
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path="/post" component={CommentBox} />
+      <Route path="/" exact component={CommentList} />
     </div>
   )
 }
