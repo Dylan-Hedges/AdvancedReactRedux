f1fd42aa511b584c85a6d2e5f8a7dd720508e7df
DylanHedges <d.hedges1@hotmail.co.uk>
2020-04-21 08:45:39 -0400

    Added sign in button - wired up Redux Store to component, added helper function that looks at the auth state (saved as a prop to the component) to see if user is signed in, displays sign in or sign out button depending on this state (prop)

--- testing/src/Components/App.js
+++ testing/src/Components/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import CommentBox from 'Components/CommentBox';
 import CommentList from 'Components/CommentList';
 
-export default () => {
-  return(
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  )
+class App extends Component {
+  //Displays sign in or sign out button - looks at auth state (saved as a prop to this component) to see if user is logged in and displays sign in or sign out button
+  renderButton(){
+    if(this.props.auth){
+      return(
+        <button>Sign Out</button>
+      );
+    }else{
+      return(
+        <button>Sign In</button>
+      );
+    }
+  }
+  //Header that displays menu options - uses React Router to navigate
+  renderHeader(){
+    return(
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="Post">Post</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+  //Displays component on screen
+  render(){
+    return(
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 }
+//Maps the Redux Store to the this component - saved under the auth prop
+function mapStateToProps(state){
+  return {auth: state.auth}
+}
+
+//Exports the component and connects it to the Redux Store
+export default connect(mapStateToProps)(App);
