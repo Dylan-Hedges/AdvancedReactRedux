9b51b22229054148f9eb0ca08ac57f57985637b1
DylanHedges <d.hedges1@hotmail.co.uk>
2020-05-15 13:46:05 -0400

    Added authentication logic to requireAuth Higher Order Component - copied auth checks from CommentBox, pasted in requireAuth, added mapStateToProps() to requireAuth, imported requireAuth into CommentBox and wrapped CommentBox inside it when exporting, passed down a copy of props {...this.props} so that CommentBox can access actions (action creators) when being rendered as a child

--- testing/src/Components/CommentBox.js
+++ testing/src/Components/CommentBox.js
@@ -3,29 +3,12 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 //Imports all action creators from actions/index.js
 import * as actions from 'actions';
+//Imports requireAuth component to be used for authentication checks (will act as parent to CommentBox)
+import requireAuth from 'Components/requireAuth';
 
 //Component that allows users to write comments - refactored to class based component so we can access component level state and watch input from a user
 class CommentBox extends Component {
   state = { comment: ''};
-
-  //Checks that the user is logged in and pushes them to the root route if not
-  shouldNavigateAway(){
-    //If the user is not logged in - user login status is mapped to the component from the Redux Store
-    if(!this.props.auth){
-      //Pushes the user to the root route - uses the React-Router method history.push() to push the user
-      this.props.history.push('/');
-    }
-  }
-
-  //When the user first visits the '/Post' route execute the check logged in helper method - Executes when component first mounts (is first rendered to the screen)
-  componentDidMount(){
-    this.shouldNavigateAway();
-  }
-
-  //When the user clicks sign out while on the '/Post' route execute the check logged in helper method - Executes when the component updates
-  componentDidUpdate(){
-    this.shouldNavigateAway();
-  }
 
   //Typing event handler - updates state based on what user types
   handleChange = (event) => {
@@ -58,9 +41,5 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state){
-  return { auth : state.auth };
-}
-
-//Wires up action creators to component - as there is no mapStateToProps we put null as the first argument
-export default connect(mapStateToProps, actions)(CommentBox);
+//Wires up action creators to component, wraps CommentBox inside of requireAuth and calls it - as there is no mapStateToProps we put null as the first argument
+export default connect(null, actions)(requireAuth(CommentBox));
--- testing/src/Components/requireAuth.js
+++ testing/src/Components/requireAuth.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-//Takes in a child component, executes logic and renders the child component
+//Takes in a child component, executes authentication checks and renders the child component
 export default ChildComponent => {
+  //Parent component class
   class ComposedComponent extends Component{
-    //Renders the child component
+      //When the user first visits the '/Post' route execute the check logged in helper method - Executes when component first mounts (is first rendered to the screen)
+      componentDidMount(){
+        this.shouldNavigateAway();
+      }
+      //When the user clicks sign out while on the '/Post' route execute the check logged in helper method - Executes when the component updates
+      componentDidUpdate(){
+        this.shouldNavigateAway();
+      }
+      //Checks that the user is logged in and pushes them to the root route if not
+      shouldNavigateAway(){
+        //If the user is not logged in - user login status is mapped to the component from the Redux Store
+        if(!this.props.auth){
+          //Pushes the user to the root route - uses the React-Router method history.push() to push the user
+          this.props.history.push('/');
+        }
+      }
+    //Renders the child component (CommentBox) and passes down from this component (parent) to CommentBox (child)
     render(){
-      return <ChildComponent />;
+      return <ChildComponent {...this.props}/>;
     }
   }
-  return ComposedComponent;
+  function mapStateToProps(state){
+    return { auth : state.auth };
+  }
+  return connect(mapStateToProps)(ComposedComponent);
 };
