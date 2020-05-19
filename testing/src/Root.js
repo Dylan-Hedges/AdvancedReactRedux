3829b029f28cd65d488f9ec6b41a7d963791efa6
DylanHedges <d.hedges1@hotmail.co.uk>
2020-05-19 08:17:20 -0400

    Wired up middleware to root component - Redux Store

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 import reducers from 'reducers';
 
 //Reuseable component that makes the Redux Store available to a component - destructures children and initialState from props; sets inital state to a blank object or whatever is passed in using the initialState prop
 export default ({ children, initialState = {} }) => {
   //Creates the Redux Store - takes in the reducers, an inital state and middleware
-  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
+  const store = createStore(reducers, initialState, applyMiddleware(async));
   //Weaps the store around the App component - {children} references the <App> component but can be swapped out for any component on the fly;
   return(
     <Provider store={store}>
--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,5 +1,5 @@
-//Redux Promise middleware recreation - function that returns a function that returns a function
-export default ({displatch}) => next => action => {
+//Redux Promise middleware recreation - middleware that handles asynchronous requests, is a function that returns a function that returns a function
+export default ({dispatch}) => next => action => {
   //Checks to see if the action has a Promise on its payload - checks if there is no payload OR the action does not have a then function, if there is no Promise it forwards the action to the next middleware in the chain
   if(!action.payload || !action.payload.then){
     //Forwards action to the next middleware in the chain
@@ -9,7 +9,7 @@ export default ({displatch}) => next => action => {
   action.payload.then(function(response){
     //Creates a new action, copies the action properies using the spread operator, overwrites the payload property with the data returned from the asynchronous request
     const newAction = {...action, payload: response};
-    //Dispatches the new action
-    displatch(newAction);
+    //Dispatches the new action - flows through all of the middlewares again
+    dispatch(newAction);
   });
 };
