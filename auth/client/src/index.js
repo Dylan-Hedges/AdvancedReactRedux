df2705f1258113c67bd2813ae2d11cd9c500637c
DylanHedges <d.hedges1@hotmail.co.uk>
2020-06-24 15:58:10 -0400

    Added boilerplate for signup page - added signup component, added <Route> for "/signup" link and linked to signup component

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 //Renders the App.js file to the DOM - <BrowserRouter> provides react-router <Link> functionality; <Route /> allows a component to be displayed inside another component on a specific route
 ReactDOM.render(
   <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
       </App>
   </BrowserRouter>,
   document.querySelector('#root')
