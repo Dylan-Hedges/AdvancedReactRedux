af692fe7ef07502325f66a3c072061daa3f360cb
DylanHedges <d.hedges1@hotmail.co.uk>
2020-06-27 13:19:10 -0400

    Added boilerplate Redux Store setup - created Redux store uainf createStore, made Redux Store accessible to app using <Provider>, added auth reducer, added combineReducers file

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import reducers from './reducers';
 
-//Renders the App.js file to the DOM - <BrowserRouter> provides react-router <Link> functionality; <Route /> allows a component to be displayed inside another component on a specific route
+//Renders the App.js file to the DOM - <BrowserRouter> provides react-router <Link> functionality; <Route /> allows a component to be displayed inside another component on a specific route; <Provider store={createStore(reducers, {})}> - creates Redux Store, passes in reducers and makes it accessible in our <App>
 ReactDOM.render(
-  <BrowserRouter>
-      <App>
-        <Route path="/" exact component={Welcome} />
-        <Route path="/signup" exact component={Signup} />
-      </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+        <App>
+          <Route path="/" exact component={Welcome} />
+          <Route path="/signup" exact component={Signup} />
+        </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
---
+++ auth/client/src/reducers/auth.js
@@ -0,0 +1,10 @@
+//Sets the inital state for auth reducer
+const INITAL_STATE={
+  authenticated: '',
+  errorMessage: ''
+};
+
+//Exports auth reducer
+export default function(state = INITAL_STATE, action){
+  return state;
+}
