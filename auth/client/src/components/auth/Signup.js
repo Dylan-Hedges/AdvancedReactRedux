976dbe037cf4986ff6332aaacebdffed60d31393
DylanHedges <d.hedges1@hotmail.co.uk>
2020-06-29 12:04:56 -0400

    Added Redux Form to Signup component - added Redux Form reducer to combineReducers call, wired up Redux Form to Signup component using reduxForm, added Redux Form <Field> components to capture user input data

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form';
 
+//Signup Component, user signs up to app - Uses reduxForm to wire up component to Redux Form, uses Redux Form <Field> component to capture user inpit
 class Signup extends Component{
   render(){
     return(
       <form>
         <fieldset>
           <label>Email</label>
+          <Field
+            name="email"
+            type="input"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
       </form>
     );
   }
 }
 
-export default Signup;
+//Exports the component and wires it up to Redux Form - {form: 'signup'} names this Redux Form signup
+export default reduxForm({form: 'signup'})(Signup);
--- auth/client/src/reducers/index.js
+++ auth/client/src/reducers/index.js
@@ -1,7 +1,12 @@
+//Imports the combine reducers call
 import {combineReducers} from 'redux';
+//Imports the Redux Form reducer used to capture form data
+import {reducer as formReducer} from 'redux-form';
+//Imports the auth reducer
 import auth from './auth';
 
 //Combines all reducers
 export default combineReducers({
-  auth: auth
+  auth: auth,
+  form: formReducer
 });
