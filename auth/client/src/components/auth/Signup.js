e7deb72bd65a7810ddec98d955ddb7a3fd428058
DylanHedges <d.hedges1@hotmail.co.uk>
2020-07-01 12:24:33 -0400

    Added functionality to the Signup component to recieve the email address and password entered in the signup form - created onSubmit function to recieve/process the username and password, added sign up button, added Redux Form handleSubmit function to <form> which executes the onSubmit function and passes the email address and password

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -3,9 +3,16 @@ import {reduxForm, Field} from 'redux-form';
 
 //Signup Component, user signs up to app - Uses reduxForm to wire up component to Redux Form, uses Redux Form <Field> component to capture user inpit
 class Signup extends Component{
+  //Recieves/processes the data the user entered in the form (email and password)
+  onSubmit = (formProps) => {
+    console.log(formProps);
+  };
   render(){
+    //Saves the handle submit fuction - this is a Redux Form function which added to the .props of the component when wiring up Redux Form to the component
+    const {handleSubmit} = this.props;
+    //Renders the sign up component - onSubmit={handleSubmit(this.onSubmit)} when the user submits the form execute the onSubmit function and pass in what the user entered
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
           <Field
@@ -24,6 +31,7 @@ class Signup extends Component{
             autoComplete="none"
           />
         </fieldset>
+        <button>Sign Up</button>
       </form>
     );
   }
