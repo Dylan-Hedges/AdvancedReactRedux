bf7477f8d0f2aa6827b6ba245af287dec0db86bd
DylanHedges <d.hedges1@hotmail.co.uk>
2020-07-02 08:40:39 -0400

    Connected Redux Thunk middleware - seperated out createStore, passed in reducers, inital state (blank) and applyMiddleware(reduxThunk) which wires up Redux Thunk to the application

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import reducers from './reducers';
 
-//Renders the App.js file to the DOM - <BrowserRouter> provides react-router <Link> functionality; <Route /> allows a component to be displayed inside another component on a specific route; <Provider store={createStore(reducers, {})}> - creates Redux Store, passes in reducers and makes it accessible in our <App>
+//Creates the Redux Store - passes in reducers, inital state (blank object) and applyMiddleware(reduxThunk) which wires up Redux Thunk to our app (allows functions to be executed in action creators)
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
+
+//Renders the App.js file to the DOM - <BrowserRouter> provides react-router <Link> functionality; <Route /> allows a component to be displayed inside another component on a specific route; <Provider store={store} /> makes the Redux Store accessible in our <App>
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={store}>
     <BrowserRouter>
         <App>
           <Route path="/" exact component={Welcome} />
