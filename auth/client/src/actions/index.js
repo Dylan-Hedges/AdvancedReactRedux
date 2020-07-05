797ae573a4e1205a37ba7a7750abeaa19a26defe
DylanHedges <d.hedges1@hotmail.co.uk>
2020-07-05 12:43:51 -0400

    Added Signup action creator boilerplate - uses Redux Thunk to execute a function (instead of returning a plain object)

---
+++ auth/client/src/actions/index.js
@@ -0,0 +1,6 @@
+import {AUTH_USER} from './types';
+
+//Signup Action Creator - uses Redux Thunk to execute a function inside this AC
+export const signup = ({email, password}) => dispatch => {
+
+};
