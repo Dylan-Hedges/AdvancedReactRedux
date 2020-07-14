6412918ae1f6ab129eb0fdd35ca3660b1f7543aa
DylanHedges <d.hedges1@hotmail.co.uk>
2020-07-14 07:38:40 -0400

    Added logic to save the JWT to the redux store - in the AC redux thunk executes the request to the bank end and save the JWT, this is dispatched in an action to the reducers, reducer takes the JWT and saves it to the Redux Store

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import {AUTH_USER} from './types';
 
 //Signup Action Creator - uses Redux Thunk to execute a function inside this AC
-export const signup = formProps => dispatch => {
-  //Sends a post request containing the users email and password to our back end (Node/Express) on local server port 3090 '/signup' route
-  axios.post('http://localhost:3090/signup', formProps);
+export const signup = formProps => async dispatch => {
+  //Saves the JWT - Sends a post request containing the users email and password to our back end (Node/Express) on local server port 3090 '/signup' route
+  const response = await axios.post('http://localhost:3090/signup', formProps);
+  //Dispatches an action with the JWT to reducers to be saved in the Redux Store- Dispatches an action with a type of AUTH_USER and a payload containing the response from the async request
+  dispatch({type: AUTH_USER, payload: response.data.token});
 };
--- auth/client/src/reducers/auth.js
+++ auth/client/src/reducers/auth.js
@@ -1,3 +1,5 @@
+//Imports the auth user action tyoe
+import {AUTH_USER} from '../actions/types';
 //Sets the inital state for auth reducer
 const INITAL_STATE={
   authenticated: '',
@@ -6,5 +8,13 @@ const INITAL_STATE={
 
 //Exports auth reducer
 export default function(state = INITAL_STATE, action){
-  return state;
+  //Switch statement determines what code to execute based on action type
+  switch(action.type){
+    //If the action type is AUTH_USER
+    case AUTH_USER:
+      //Add the users JWT to the Redux Store - returns all the properties of the state object + the JWT under the authenticated K/V pair
+      return{...state, authenticated: action.payload};
+    default:
+      return state;
+  }
 }
